3028b1ffe7cd0697115b0f653e1ac8558f45a337
Jonathan Asquith <jonathan_d_asquith@hotmail.com>
2022-11-02 23:22:14 +0000

    Add await vs return task benchmarks

---
+++ src/BenchmarkTaskAwaitVsTaskReturn/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkTaskAwaitVsTaskReturn;
+
+BenchmarkRunner.Run<TaskBenchmarks>();
---
+++ src/BenchmarkTaskAwaitVsTaskReturn/TaskBenchmarks.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkTaskAwaitVsTaskReturn;
+
+//[RPlotExporter]
+[MemoryDiagnoser, Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class TaskBenchmarks
+{
+    [Benchmark]
+    public async Task<int> AwaitTask() => await GetInt();
+
+    [Benchmark]
+    public Task<int> ReturnTask() => GetInt();
+
+    private Task<int> GetInt() => Task.FromResult(3);
+}
