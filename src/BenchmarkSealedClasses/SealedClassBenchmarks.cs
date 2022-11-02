3b96f9b3c58d2c879e115f0e0c0f72803298d107
Jonathan Asquith <jonathan_d_asquith@hotmail.com>
2022-11-02 23:16:11 +0000

    Add open vs sealed class benchmarks

---
+++ src/BenchmarkSealedClasses/SealedClassBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkSealedClasses;
+
+//[RPlotExporter]
+//[MemoryDiagnoser, Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class SealedClassBenchmarks
+{
+    private readonly BaseClass _baseClass = new();
+    private readonly OpenClass _openClass = new();
+    private readonly SealedClass _sealedClass = new();
+
+    [Benchmark]
+    public void OpenVoid() => _openClass.ExampleVoidMethod();
+
+    [Benchmark]
+    public void SealedVoid() => _sealedClass.ExampleVoidMethod();
+
+
+    [Benchmark]
+    public int OpenInt() => _openClass.ExampleIntMethod() + 1337;
+
+    [Benchmark]
+    public int SealedInt() => _sealedClass.ExampleIntMethod() + 1337;
+
+
+    [Benchmark]
+    public bool OpenIsCheck() => _openClass is OpenClass;
+
+    [Benchmark]
+    public bool SealedIsCheck() => _sealedClass is SealedClass;
+}
+
+public class BaseClass
+{
+    public virtual void ExampleVoidMethod() { }
+    public virtual int ExampleIntMethod() => 1;
+}
+
+public class OpenClass : BaseClass
+{
+    public override void ExampleVoidMethod() { }
+    public override int ExampleIntMethod() => 2;
+}
+
+public sealed class SealedClass : BaseClass
+{
+    public override void ExampleVoidMethod() { }
+    public override int ExampleIntMethod() => 2;
+}
